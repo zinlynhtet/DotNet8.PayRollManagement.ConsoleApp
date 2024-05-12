a397ee4225dc60d8afb031f1348becb754baa66a
Zin Linn Htet <mackk5504@gmail.com>
2024-05-12 23:19:15 +0630

    -

--- DotNet8.PayRollManagement.ConsoleApp/DevCodes.cs
+++ DotNet8.PayRollManagement.ConsoleApp/DevCodes.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.PayRollManagement.ConsoleApp;
 
-namespace DotNet8.PayRollManagement.ConsoleApp
+public static class DevCodes
 {
-    public static class DevCodes
+    public static int ToInt(this object item)
     {
-        public static int ToInt(this object item)
-        {
-            return Convert.ToInt32(Console.ReadLine());
-        }
+        return Convert.ToInt32(item);
+    }
 
-        public static double ToDouble(this object item)
-        {
-            return Convert.ToDouble(Console.ReadLine());
-        }
+    public static double ToDouble(this object item)
+    {
+        return Convert.ToDouble(item);
     }
 }
