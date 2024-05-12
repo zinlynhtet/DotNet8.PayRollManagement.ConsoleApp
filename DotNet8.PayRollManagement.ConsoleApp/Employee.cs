102683c69aac0fd3acd01d21c23a88b39cf7a53c
Zin Linn Htet <mackk5504@gmail.com>
2024-05-12 23:10:17 +0630

    Add Employee Model

---
+++ DotNet8.PayRollManagement.ConsoleApp/Employee.cs
@@ -0,0 +1,24 @@
+﻿public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double HourlyRate { get; set; }
+    public double HoursWorked { get; set; }
+    public Employee(int id, string name, double hourlyRate, double hoursWorked)
+    {
+        Id = id;
+        Name = name;
+        HourlyRate = hourlyRate;
+        HoursWorked = hoursWorked;
+    }
+
+    public double CalculatePay()
+    {
+        if (HourlyRate < 0 || HoursWorked < 0)
+            throw new InvalidOperationException("Hourly rate and hours worked must be non-negative.");
+
+        return HourlyRate * HoursWorked;
+    }
+    
+}
+
