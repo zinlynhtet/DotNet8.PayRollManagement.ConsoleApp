32ec8ae90a443ff921fd7e7a3b834713b8487231
Zin Linn Htet <mackk5504@gmail.com>
2024-05-12 23:10:05 +0630

    Add DevCodes

---
+++ DotNet8.PayRollManagement.ConsoleApp/DevCodes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PayRollManagement.ConsoleApp
+{
+    public static class DevCodes
+    {
+        public static int ToInt(this object item)
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static double ToDouble(this object item)
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
